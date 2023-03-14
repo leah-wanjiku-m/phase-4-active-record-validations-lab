20a91d81acbae665379bd954ea5f01bd919de0e7
leah wanjiku <leagm22@gmail.com>
2023-03-14 19:10:00 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :is_expected_not_to_allow_value
+    def is_expected_not_to_allow_value
+        if title== "True Facts"
+      errors.add(:title, "Title not permitted")
+    end
+end
 end
